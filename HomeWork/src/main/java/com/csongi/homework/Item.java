d55aada40539f08eb97382ea910e604b8b7c45f2
Csongi <csongorkiss94@gmail.com>
2023-03-25 14:13:33 +0100

    FirstTry

--- HomeWork/src/main/java/com/csongi/homework/Item.java
+++ HomeWork/src/main/java/com/csongi/homework/Item.java
@@ -10,5 +10,21 @@ public class Item {
         this.price = price;
         savedMoney = 0;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
     
 }
