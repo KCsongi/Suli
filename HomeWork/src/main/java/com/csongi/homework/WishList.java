19f892291e4caf8aa7ca59e7b5a6161786b3afc0
Csongi <csongorkiss94@gmail.com>
2023-03-25 17:02:36 +0100

    Item

--- HomeWork/src/main/java/com/csongi/homework/WishList.java
+++ HomeWork/src/main/java/com/csongi/homework/WishList.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 public class WishList {
    
     ArrayList<Item> items;
+
+    public WishList() {
+        this.items = new ArrayList<>();
+    }
+    
     
     public void addItem(String name, int price) {
         Item item = new Item(name, price);
@@ -12,7 +17,8 @@ public class WishList {
     }
     public void getItem() {
         for(Item item : items) {
-            System.out.println("A kosárba lévő termékek: " + item);
+            System.out.println("A kosárba lévő termékek: " + item.getName() + " az ára: " + item.getPrice());
         }
     }
+    
 }
