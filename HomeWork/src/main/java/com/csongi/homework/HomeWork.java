5a1a3613a188e9cd21d18c78a3aa60db1f4faf72
Csongi <csongorkiss94@gmail.com>
2023-03-25 14:11:47 +0100

    Első

--- HomeWork/src/main/java/com/csongi/homework/HomeWork.java
+++ HomeWork/src/main/java/com/csongi/homework/HomeWork.java
@@ -1,8 +1,19 @@
 package com.csongi.homework;
 
+import java.util.Scanner;
+
 public class HomeWork {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Üdvözlöm. Kérjem írjon be egy kívánságot: ");
+        String wish = scanner.nextLine();
+        
+        System.out.println("Kérem írja be az árát: ");
+        int price = scanner.nextInt();
+        
+        WishList list = new WishList();
+        list.addItem(wish, price);
     }
 }
---
+++ HomeWork/src/main/java/com/csongi/homework/WishList.java
@@ -0,0 +1,16 @@
+package com.csongi.homework;
+
+import java.util.ArrayList;
+
+public class WishList {
+   
+    ArrayList<Item> items;
+    
+    public void addItem(String name, int price) {
+        Item item = new Item(name, price);
+        items.add(item);
+    }
+    public void getItem() {
+        
+    }
+}
